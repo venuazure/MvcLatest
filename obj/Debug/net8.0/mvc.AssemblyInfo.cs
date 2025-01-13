a37b6f76ab0a9cae48c23b6fbfd21d95db037f13
vijay <velagav4@outlook.com>
2025-01-13 20:19:30 +0530

    changed pipeline code

--- obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b14993ced11e3775bf6ef48425c62c6947b3a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ae9389e420b5098f24a01a3b365cea37c6546c")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
