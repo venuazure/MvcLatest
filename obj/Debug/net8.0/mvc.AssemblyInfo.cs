d2424801a0f8fc979f8bc67c546ac1f89c6bb21f
vijay <velagav4@outlook.com>
2025-01-14 11:46:49 +0530

    changed pipelines.yaml file

--- obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ae9389e420b5098f24a01a3b365cea37c6546c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4ef4a274942271f55f14b22b92e32f71348d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
